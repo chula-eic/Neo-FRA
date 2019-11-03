1e70806e575e84820c0d5b26362eff906fa15186
Strik3agle98 <can_saelo@hotmail.com>
2019-11-03 21:26:42 +0700

    edited webcam client

--- WebCamClient.py
+++ WebCamClient.py
@@ -6,12 +6,16 @@ import pickle
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import numpy as np
 camera = PiCamera()
 camera.resolution = (320, 240)
+camera.framerate = 24
 rawCapture = PiRGBArray(camera)
-
+print('initializing completed')
+print('establishing connection')
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.2.38', 8080))
+client_socket.connect(('192.168.2.38', 8554))
+print('connected')
 connection = client_socket.makefile('wb')
 
 
@@ -24,11 +28,12 @@ connection = client_socket.makefile('wb')
 img_counter = 0
 
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-
+    
 while True:
     #ret, frame = cam.read()
-    camera.capture(rawCapture, format="bgr")
-    frame = rawCapture.array
+    frame = np.empty((240, 320, 3), dtype=np.uint8)
+    camera.capture(frame, 'bgr')
+    print(frame.shape)
     result, frame = cv2.imencode('.jpg', frame, encode_param)
 #    data = zlib.compress(pickle.dumps(frame, 0))
     data = pickle.dumps(frame, 0)
