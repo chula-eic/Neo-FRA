1be35966ae320fba762c53ce9e905be53d5240c5
Strik3agle98 <can_saelo@hotmail.com>
2019-11-12 13:54:58 +0700

    debug Multiprocess for PI

--- Manual/main_PI.py
+++ Manual/main_PI.py
@@ -1,11 +1,12 @@
 from multiprocessing import Process
-import PCsocket
-import WebCamServer
+import PIsocket
+import WebCamClient
 
+IP = '172.16.0.126'
 if __name__ == "__main__":
     try:
-        p1 = Process(target=PCsocket.start, args=(6783,))
-        p2 = Process(target=WebCamServer.start, args=(8554,))
+        p1 = Process(target=PIsocket.start, args=(IP, 6783))
+        p2 = Process(target=WebCamClient.start, args=(IP, 8554))
         p1.start()
         p2.start()
         p1.join()
